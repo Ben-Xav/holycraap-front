1ac851ec52c05d39ac6430fb75a3532bb69ce69c
Benjamin Morant <bmorant@oxiane.com>
2020-06-22 11:41:00 +0200

    modified App.js and added proxy to package.json

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    isLoading: true,
+    groups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/people');
+    const body = await response.json();
+    this.setState({ people: body, isLoading: false });
+  }
+
   render() {
+    const {people, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="App">
-        <div className="App-header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="App-intro">
+            <h2>JUG List</h2>
+            {people.map(people =>
+              <div key={people.id}>
+                {people.name}
+              </div>
+            )}
+          </div>
+        </header>
       </div>
     );
   }
